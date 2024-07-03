7bfe2f2b12af54c1af385e12bcb40f36ff7e5fde
Rudra Prasad <rudraofficial202@gmail.com>
2024-07-03 23:16:35 +0530

    imported card

--- app/colleges/page.tsx
+++ app/colleges/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const page = () => {
+  const router = useRouter();
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex justify-center gap-5">
+      <Card className=" w-1/2">
+        <CardHeader>
+          <CardTitle>ROLAND INSTITUTE OF TECHNOLOGY</CardTitle>
+          <CardDescription>2023</CardDescription>
+        </CardHeader>
+        <CardContent className="flex gap-2">
+          <Button onClick={() => router.push("/colleges/roland/1")}>
+            SEM 1
+          </Button>
+          <Button onClick={() => router.push("/colleges/roland/2")}>
+            SEM 2
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default page
+export default page;
