ea34e38da8052170bcdc6fc91e301a38ccf4d0d3
Rudra Prasad <rudraofficial202@gmail.com>
2024-07-04 21:32:02 +0530

    fixed card in small screen

--- app/colleges/page.tsx
+++ app/colleges/page.tsx
@@ -13,13 +13,14 @@ import React from "react";
 const Colleges = () => {
   const router = useRouter();
   return (
-    <div className="flex justify-center gap-5">
-      <Card className=" w-1/2">
+    <div className="grid justify-between gap-5 grid-cols-1 sm:grid-cols-2">
+      <Card className="hover:shadow-lg">
         <CardHeader>
           <CardTitle>ROLAND INSTITUTE OF TECHNOLOGY</CardTitle>
-          <CardDescription>2023</CardDescription>
+          <p>2023</p>
         </CardHeader>
         <CardContent className="flex gap-2">
+        
           <Button onClick={() => router.push("/colleges/roland/1")}>
             SEM 1
           </Button>
