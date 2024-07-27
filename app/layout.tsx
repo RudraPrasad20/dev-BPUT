69201a5141aae3607f24c5b65275690153eb1725
Rudra Prasad <rudraofficial202@gmail.com>
2024-07-27 07:40:32 +0530

    new search bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className=" m-6">
+          <Nav/>
           {children}
         </main>
       </body>
